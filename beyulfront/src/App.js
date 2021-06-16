51b19d346e13b43fe9e2de1b66630e09d7e4c5d5
Nischal Khatri <nischal06khatri@gmail.com>
2021-06-16 22:15:34 +0545

    Setting up the react app

---
+++ beyulfront/src/App.js
@@ -0,0 +1,13 @@
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello Beyul Venture</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
