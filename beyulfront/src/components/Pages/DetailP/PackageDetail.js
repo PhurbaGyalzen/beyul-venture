2d246a1450000ff0d020a9b2063e19de94b7b87a
Nischal Khatri <nischal06khatri@gmail.com>
2021-06-28 19:33:08 +0545

    Added hover-zoom effect

---
+++ beyulfront/src/components/Pages/DetailP/PackageDetail.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PackageDetail = (props) => {
+    return (
+        <div>
+            <h2>sjabfiajskdndjm</h2>
+        </div>
+    )
+}
+
+
+
+export default PackageDetail
--- beyulfront/src/components/Pages/Landing/Package.js
+++ beyulfront/src/components/Pages/Landing/Package.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import {Grid, Card, CardContent,CardMedia ,Typography,makeStyles} from '@material-ui/core';
-
+import './landingstyle.css'
 
 const useStyles = makeStyles((theme)=>({
     cardImage:{
-
+        height:"15rem",
+        overflow: "hidden"
     },
     card:{
         height: '100%',
         display: 'Flex',
         flexDirection : 'column'
+
     },
     cardCont:{
         height: "40px",
@@ -23,12 +25,15 @@ const Package = (props) => {
     return (
         <Grid item key={props.data.id} xs={12} md={4} sm={6}>
             <Card className={classes.card}>
-                <CardMedia 
-                className={classes.cardImage}
-                image ={props.data.image}
-                title={props.data.title} 
-                style={{height: "3rem", paddingTop: '56.25%'}}
-                />
+                <div className={classes.cardImage}>
+                    <CardMedia
+                    component="img" 
+                    className={classes.cardImage}
+                    image ={props.data.image}
+                    title={props.data.title} 
+                    // style={{height: "3rem", paddingTop: '56.25%'}}
+                    />
+                </div>
                 <CardContent classname={classes.cardCont}>
                     <Typography gutterBottom variant="h5" align="justify">
                         {props.data.title}
