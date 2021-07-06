b878059b9314be5560bae36b5d14c6fa62dc86b9
PhurbaGyalzen <sphurba861@gmail.com>
2021-07-06 10:09:30 +0545

    api endpoints set for jwt authentication

--- beyulback/beyul/urls.py
+++ beyulback/beyul/urls.py
@@ -4,6 +4,10 @@ from rest_framework.routers import DefaultRouter
 from blog import views
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 router = DefaultRouter()
 router.register(r'blog', views.BlogView)
@@ -11,4 +15,6 @@ router.register(r'blog', views.BlogView)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
