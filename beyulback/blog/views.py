700cbaf87e96acc7de9c92edcad98f4959031576
PhurbaGyalzen <sphurba861@gmail.com>
2021-07-06 10:10:50 +0545

    jwt implemented for blog view

--- beyulback/blog/views.py
+++ beyulback/blog/views.py
@@ -1,8 +1,13 @@
 from rest_framework import viewsets
 from .models import Blog
 from .serializers import BlogSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 
 class BlogView(viewsets.ModelViewSet):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
+    # access the view functions,if a valid token is provided
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]  # user must be logged in
