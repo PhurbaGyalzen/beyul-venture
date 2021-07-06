712cca17c164dd3abc80526245aeb517f1bcb53e
zznixt07 <zznixt07@protonmail.com>
2021-07-06 11:41:39 +0545

    limbus about us images

---
+++ beyulfront/src/components/Copyright.js
@@ -0,0 +1,18 @@
+import Link from '@material-ui/core/Link'
+import Typography from '@material-ui/core/Typography'
+
+function Copyright() {
+  console.log(process.env)
+  return (
+    <Typography variant='body2' color='textSecondary' align='center'>
+      {'Copyright © '}
+      <Link color='inherit' href={process.env.PUBLIC_URL}>
+        Beyul Venture
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  )
+}
+
+export default Copyright
