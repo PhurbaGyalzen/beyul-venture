120b1047c4fd89c9ffd7fa710fd602e6d94c07a9
PhurbaGyalzen <sphurba861@gmail.com>
2021-07-07 08:00:32 +0545

    auto slug and slugify added

--- beyulback/blog/models.py
+++ beyulback/blog/models.py
@@ -1,6 +1,9 @@
+
 from datetime import datetime, timezone
 from django.db import models
 from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
+from django.urls import reverse
 
 
 STATUS = (
@@ -8,9 +11,10 @@ STATUS = (
     (1, "Publish")
 )
 
+
 class Blog(models.Model):
     title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200, unique=True)
+    slug = models.SlugField(blank=True, unique=True)
     description = models.CharField(max_length=500, blank=True)
     # JSONField is supported on MariaDB 10.2.7+, MySQL 5.7.8+, Oracle,
     # PostgreSQL, and SQLite 3.9.0+ (with the JSON1 extension enabled).
@@ -25,11 +29,20 @@ class Blog(models.Model):
         blank=True,
         null=True
     )
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        User, related_name="post", on_delete=models.CASCADE)
     status = models.IntegerField(choices=STATUS, default=0)
 
     class Meta:
         ordering = ['-created_on']
+
+    def get_absolute_url(self):
+        return reverse('blog_detail', kwargs={'slug': self.slug})
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
