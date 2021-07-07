bcc9891da68102ad3e3bdaf0aefa0e026f597662
PhurbaGyalzen <sphurba861@gmail.com>
2021-07-07 08:25:50 +0545

    main url updated for generating token

--- beyulback/beyul/urls.py
+++ beyulback/beyul/urls.py
@@ -1,20 +1,23 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from blog import views
+from blog.views import (
+    CustomObtainTokenPairView,
+    BlogView,
+    UserViewSet,
+)
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from rest_framework_simplejwt.views import TokenRefreshView
 
 router = DefaultRouter()
-router.register(r'blog', views.BlogView)
+router.register(r'blog', BlogView)
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/', CustomObtainTokenPairView.as_view(),
+         name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
