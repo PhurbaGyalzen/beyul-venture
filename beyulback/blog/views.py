9c19d4d5ab3495a805d9b3c8516644cf712289e3
PhurbaGyalzen <sphurba861@gmail.com>
2021-07-07 08:22:42 +0545

    CustomObtainTokenPairView created

--- beyulback/blog/views.py
+++ beyulback/blog/views.py
@@ -1,8 +1,16 @@
 from rest_framework import viewsets
 from .models import Blog
-from .serializers import BlogSerializer
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from django.contrib.auth.models import User
+from .serializers import BlogSerializer, UserSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import CustomTokenObtainPairSerializer
+
+
+class CustomObtainTokenPairView(TokenObtainPairView):
+    permission_classes = (AllowAny,)
+    serializer_class = CustomTokenObtainPairSerializer
 
 
 class BlogView(viewsets.ModelViewSet):
@@ -12,3 +20,9 @@ class BlogView(viewsets.ModelViewSet):
     authentication_classes = [JWTAuthentication]
     # anyone can read the post but user must be logged in to post or delete
     permission_classes = [IsAuthenticatedOrReadOnly]
+    lookup_field = 'slug'
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
