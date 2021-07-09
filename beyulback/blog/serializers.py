3a20090c81d1bf70e8e064f603ab9bd319d73039
PhurbaGyalzen <sphurba861@gmail.com>
2021-07-09 23:33:23 +0545

    author changed to CustomUser and more fileds where added to serializer

--- beyulback/blog/serializers.py
+++ beyulback/blog/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from .models import Blog
-from django.contrib.auth.models import User
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from django.contrib.auth import get_user_model
+User = get_user_model()  # User is now the CustomUser
 
 
 class BlogSerializer(serializers.HyperlinkedModelSerializer):
@@ -46,7 +47,7 @@ class UserSerializer(ReadOnlyModelSerializer):
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'posts']
+        fields = ['id', 'email', 'first_name', 'profile_pic', 'posts']
         # Alternative way of making all the fields read only
         # use case: put the below field in side any Model serializer's meta class
         # read_only_fields = [f.name for f in MyModel._meta.get_fields()]
@@ -58,6 +59,7 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     def get_token(cls, user):
         token = super(CustomTokenObtainPairSerializer, cls).get_token(user)
 
-        # to obtain the username from token
-        token['username'] = user.username
+        # to obtain the email and first_name from token
+        token['email'] = user.email
+        token['first_name'] = user.first_name
         return token
