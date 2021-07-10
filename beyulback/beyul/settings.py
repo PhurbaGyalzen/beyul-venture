f3387e547842a0f1a806010fb92e7cee50dd2eda
PhurbaGyalzen <sphurba861@gmail.com>
2021-07-10 10:05:07 +0545

    Default Pagination added to all View

--- beyulback/beyul/settings.py
+++ beyulback/beyul/settings.py
@@ -138,7 +138,8 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'blog.custompaginations.CustomPageNumberPagination',
 }
 
 CORS_ALLOWED_ORIGINS = [
