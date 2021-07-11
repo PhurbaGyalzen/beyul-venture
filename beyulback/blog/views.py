0f03639380270e9f7db8347c3ed3e175a3fc77b1
PhurbaGyalzen <sphurba861@gmail.com>
2021-07-11 10:10:00 +0545

    TagViewSet added

--- beyulback/blog/views.py
+++ beyulback/blog/views.py
@@ -1,12 +1,19 @@
-from .models import Blog
+from .models import Blog, Tag
 from .serializers import CustomTokenObtainPairSerializer
-from .serializers import BlogSerializer, UserSerializer
+from .serializers import (
+    BlogSerializer,
+    UserSerializer,
+    TagSerializer,
+)
 from .custompaginations import RemovePageNumberPagination
+
+from django.contrib.auth import get_user_model
+
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.views import TokenObtainPairView
-from django.contrib.auth import get_user_model
+
 User = get_user_model()  # User is now the CustomUser
 
 
@@ -30,3 +37,10 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     pagination_class = RemovePageNumberPagination
+
+
+class TagViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    pagination_class = RemovePageNumberPagination
+    lookup_field = 'slug'
