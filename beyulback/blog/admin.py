354a982605519945c0dc196017e7a3879fd7fa80
PhurbaGyalzen <sphurba861@gmail.com>
2021-07-11 16:59:32 +0545

    Clap registered on admin.site

--- beyulback/blog/admin.py
+++ beyulback/blog/admin.py
@@ -1,7 +1,8 @@
-from .models import Blog, Tag, Comment
+from .models import Blog, Tag, Comment, Clap
 
 from django.contrib import admin
 
 admin.site.register(Blog)
 admin.site.register(Tag)
 admin.site.register(Comment)
+admin.site.register(Clap)
