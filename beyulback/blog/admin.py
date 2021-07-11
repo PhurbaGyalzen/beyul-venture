3af0c02dae874844cfbfa7d22486250195ce98c6
PhurbaGyalzen <sphurba861@gmail.com>
2021-07-11 14:14:27 +0545

    Comment Model registered in admin.site

--- beyulback/blog/admin.py
+++ beyulback/blog/admin.py
@@ -1,6 +1,7 @@
-from .models import Blog, Tag
+from .models import Blog, Tag, Comment
 
 from django.contrib import admin
 
 admin.site.register(Blog)
 admin.site.register(Tag)
+admin.site.register(Comment)
