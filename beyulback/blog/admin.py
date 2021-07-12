71ff89ab798ef7fe47aa77817bd542049ada16da
PhurbaGyalzen <sphurba861@gmail.com>
2021-07-12 08:35:56 +0545

    mptt.admin added

--- beyulback/blog/admin.py
+++ beyulback/blog/admin.py
@@ -1,8 +1,10 @@
 from .models import Blog, Tag, Comment, Clap
 
+from mptt.admin import MPTTModelAdmin
+
 from django.contrib import admin
 
 admin.site.register(Blog)
 admin.site.register(Tag)
-admin.site.register(Comment)
+admin.site.register(Comment, MPTTModelAdmin)
 admin.site.register(Clap)
