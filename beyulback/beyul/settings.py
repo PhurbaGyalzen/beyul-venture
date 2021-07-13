7adb0422dd06dd0c57ac42df7377c3caf0847877
Nischal Khatri <nischal06khatri@gmail.com>
2021-07-13 18:05:10 +0545

    Added localhost in CORS allowed origin in settings.py to access api data

--- beyulback/beyul/settings.py
+++ beyulback/beyul/settings.py
@@ -146,7 +146,8 @@ REST_FRAMEWORK = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000",
+    "http://localhost:3000"
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
