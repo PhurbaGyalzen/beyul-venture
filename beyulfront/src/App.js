7fad9b0514f2533f48927bcdc762fc8234911141
zznixt07 <zznixt07@protonmail.com>
2021-07-16 23:54:14 +0545

    added 404 page

--- beyulfront/src/App.js
+++ beyulfront/src/App.js
@@ -7,20 +7,19 @@ import {
 import Blog from 'pages/blog-page/Blog'
 import AboutPage from 'pages/AboutPage'
 import HomePage from 'pages/landing/HomePage'
-import AboutUsPage from './pages/AboutUsPage'
-
+import AboutUsPage from 'pages/AboutUsPage'
 import BlogDetail from 'pages/blog-page/BlogDetail'
 import PackageDetail from 'pages/package-detail/PackageDetail'
 import Header from 'components/Header'
 import SignIn from 'components/SignIn'
 import SignUp from 'components/SignUp'
-import { AddBlog } from './pages/blog-add/AddBlog'
-import { Footer } from './components/Footer'
-import ContactUs from './pages/ContactUs.js'
-import ajax from './api'
-import { BlogTag } from './pages/blog-page/BlogTag'
-
+import { AddBlog } from 'pages/blog-add/AddBlog'
+import { Footer } from 'components/Footer'
+import ContactUs from 'pages/ContactUs.js'
+import { BlogTag } from 'pages/blog-page/BlogTag'
+import FourZeroFour from 'pages/404'
 // import { default as SignUp } from 'components/SignUpDialog'
+import ajax from './api'
 
 window.ajax = ajax
 
@@ -36,13 +35,13 @@ const Routes = () => {
       <Route exact path='/sign-in' component={SignIn} />
       <Route exact path='/blog' component={Blog} />
       <Route exact path='/blog/add' component={AddBlog} />
-      <Route exact path= '/blog/tag/:tagname' component={BlogTag} />
+      <Route exact path='/blog/tag/:tagname' component={BlogTag} />
       <Route exact path='/blog/:blogid' component={BlogDetail} />
       <Route exact path='/contact-us' component={ContactUs} />
-
       <Route exact path='/'>
         <HomePage />
       </Route>
+      <Route path='/' component={FourZeroFour} />
     </Switch>
   )
 }
---
+++ beyulfront/src/pages/404.js
@@ -0,0 +1,110 @@
+import { useRef, useState, useEffect } from 'react'
+import styled from 'styled-components'
+import bg404 from 'img/404 (1).jpg'
+// import bg404 from 'img/404 (4).jpg'
+
+const CompassContainer = styled.div`
+    height: 100vh;
+    padding: 6rem 1rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-image: url('${bg404}');
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+`
+
+const CompassDegree = styled.p`
+    font-size: 3rem;
+    font-weight: 600;
+    color: white;
+    margin: 0;
+`
+
+const Compass = styled.div`
+    position: relative;
+    max-width: 18rem;
+    max-height: 18rem;
+`
+
+const Needle = styled.img`
+    width: 100%;
+    transition: transform 2s ease-in-out;
+`
+
+const Rim = styled.img`
+    width: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+`
+
+const FourZeroFour = (props) => {
+    const needleRef = useRef(null)
+    const [needle, setNeedle] = useState(null)
+    const [angle, setAngle] = useState(1)
+    const mul = 300
+    const [middlePoints, setMiddlePoints] = useState([])
+
+    useEffect(() => {
+        const ref = needleRef.current
+        if (!ref) return
+        setNeedle(ref)
+    }, [needleRef])
+
+    useEffect(() => {
+        if (!needle) return
+        // show animation at the begining of component render
+        needle.style.transform = 'rotate(860deg)'
+        setMiddlePoints([
+            needle.getBoundingClientRect().x + needle.clientWidth / 2,
+            needle.getBoundingClientRect().y + needle.clientHeight / 2,
+        ])
+    }, [needle])
+
+    const animate = (event) => {
+        const diffX = event.movementX
+        const diffY = event.movementY
+        let degree
+        if (Math.abs(diffY) >= Math.abs(diffX)) {
+            if (
+                (diffY >= 0 && event.clientX >= middlePoints[0]) ||
+                (diffY <= 0 && event.clientX <= middlePoints[0])
+            ) {
+                degree = angle + Math.abs(diffY)
+            } else {
+                degree = angle - Math.abs(diffY)
+            }
+            degree = degree * 0.012
+            needle.style.transform = `rotate(${degree * mul}deg)`
+        } else {
+            if (
+                (diffX >= 0 && event.clientY <= middlePoints[1]) ||
+                (diffX < 0 && event.clientY > middlePoints[1])
+            ) {
+                degree = angle + Math.abs(diffX)
+            } else {
+                degree = angle - Math.abs(diffX)
+            }
+            degree = degree * 0.012
+            needle.style.transform = `rotate(${degree * mul}deg)`
+        }
+        setAngle(degree)
+    }
+
+    return (
+        <CompassContainer>
+            <Compass onMouseMove={animate}>
+                <Needle
+                    ref={needleRef}
+                    src='https://maptia.imgix.net/assets/images/404/compass-arrow.png?w=300&dpr=1.5&cs=srgb'
+                />
+                <Rim src='https://maptia.imgix.net/assets/images/404/compass-rim.png?w=300&dpr=1.5&cs=srgb' />
+            </Compass>
+            <CompassDegree>404°N</CompassDegree>
+        </CompassContainer>
+    )
+}
+
+export default FourZeroFour
