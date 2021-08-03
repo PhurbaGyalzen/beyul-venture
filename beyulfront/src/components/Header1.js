92d2c94f7268d7433f994d3697c982d3e1711397
Sunil tamang <technicalnepal31@gmail.com>
2021-08-03 11:32:44 +0545

    Appbar edited

--- beyulfront/src/components/Header1.js
+++ beyulfront/src/components/Header1.js
@@ -1,58 +1,125 @@
-import { useEffect } from 'react'
-import { makeStyles } from '@material-ui/core/styles'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
+import React, {useState} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import { useEffect } from 'react';
+import SortIcon from '@material-ui/icons/Sort';
 import {
-  CssBaseline,
-  Container,
-  Button,
-  IconButton,
-  Menu,
-  MenuItem,
-} from '@material-ui/core'
-import MenuIcon from '@material-ui/icons/Menu'
-import styled from 'styled-components'
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText
+} 
+from '@material-ui/core'
 import { Link, NavLink } from 'react-router-dom'
-import { OpenDialog } from './SignUp'
-const useStyles = makeStyles((theme) => {
-  // console.log({theme});
-  return {
-    // appBar: {
-      // borderBottom: `1px solid ${theme.palette.divider}`,
-    //   height: '4rem',
-    //   boxShadow: 'none',
-    //   backgroundColor: '#13181e',
-    // },
-    // root: {
-    //   width: '100%',
-    // },
-    // navTitle: {
-    //   // color: "#60DEF7",
-    //   color: '#13181e',
-    //   fontFamily: 'sans-serif',
-    //   fontWeight: '900',
-    //   fontSize: '1.5rem',
-    //   flexGrow: 1,
-    // },
-    //   signUp: {
-    //     backgroundColor: theme.palette.primary[300],
-    //   },
-    //   navBtn: {
-    //     color: '#fffff',
-    //     fontWeight: 'bold',
-    //   }
-    
+import { List as ListIcon, Label as LabelIcon} from '@material-ui/icons';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1
+  },
+
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+
+  title: {
+    flexGrow: 1,
+    color:"#c47d1e",
+    fontSize:"1.5rem",
+    fontWeight:"bold",
+    letterSpacing:"0.125rem",
+    marginLeft:"1.25rem"
+  },
+
+  button:{
+    color:"#ffffff",
+    backgroundColor:"#13181e",
+    marginRight:"1.25rem",
+    fontWeight:"bold",
+    width:"6rem",
+    '&:hover': {
+      color: "#ffffff",
+      cursor:"pointer",
+      backgroundColor:"#c47d1e"
+    }
+  },
+
+  login:{
+    backgroundColor:"#13181e",
+    color:"#ffffff",
+    boxShadow:0,
+    marginRight:"1.25rem",
+    fontWeight:"bold",
+    '&:hover': {
+      color: "#ffffff",
+      cursor:"pointer",
+      backgroundColor:"#c47d1e",
+    }
+
+  },
+
+  signUp:{
+    backgroundColor:"#c47d1e",
+    color:"#ffffff",
+    marginRight:"1.25rem",
+    fontWeight:"bold",
+    '&:hover': {
+      color: "#ffffff",
+      cursor:"pointer",
+      backgroundColor:"#c47d1e",
+    }
+
+  },
+
+  sectionDesktop:{
+    display:'none',
+    [theme.breakpoints.up("md")]:{
+      display:"flex"
+    }
+  },
+
   
-}})
+
+  menuIcon:{
+    marginRight: theme.spacing(2),
+    display:'none',
+    [theme.breakpoints.down('md')]:{
+      display:"flex"
+    }},
+
+  list:{
+    width: '200px',
+  },
+
+  link:{
+    textDecoration: 'none',
+    color:"#ffffff",
+  },
+
+  appBarTransparent: {
+    backgroundColor: 'rgba(10, 10, 10,0)',
+    transition: 'backgroundColor 1s easeInOut'
+  },
+
+  appBarSolid: {
+    backgroundColor:'#13181e'
+  }
+
+}));
 
 const NavBtn = (props) => {
   return (
     <Button
       variant='outlined'
       component={NavLink}
-      style={{ fontWeight: 'bold', color: '#000' }}
-      activeStyle={{ border: '2px solid white' }}
+      activeStyle={{ border: '2px solid #c47d1e' }}
       exact
       {...props}
     >
@@ -61,46 +128,138 @@ const NavBtn = (props) => {
   )
 }
 
-const Header = () => {
-  const classes = useStyles()
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+  const [navBackground, setNavBackground] = useState('appBarTransparent')
+  const navRef = React.useRef()
+  navRef.current = navBackground
+
+  useEffect(() => {
+    const handleScroll = () => {
+        const show = window.scrollY > 310
+        if (show) {
+            setNavBackground('appBarSolid')
+        } else {
+            setNavBackground('appBarTransparent')
+        }
+    }
+    document.addEventListener('scroll', handleScroll)
+    return () => {
+        document.removeEventListener('scroll', handleScroll)
+    }
+}, [])
+
+  //state
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  //functions
+  const toggleDrawer = () => {
+      setDrawerOpen(!drawerOpen);
+  };
+
+  const drawerItems = [
+      {
+          text: 'Home',
+          icon: <ListIcon/>,
+          link: '/',
+      },
+      {
+          text: 'About',
+          icon: <LabelIcon/>,
+          link: '/about',
+      },
+
+      {
+        text: 'Blog',
+        icon: <LabelIcon/>,
+        link: '/blog',
+      },
+
+      {
+        text: 'Contact',
+        icon: <LabelIcon/>,
+        link: '/contact-us',
+      },
+
+      {
+        text: 'Team',
+        icon: <LabelIcon/>,
+        link: '/our-team',
+      },
+
+      {
+        text: 'Log In',
+        icon: <LabelIcon/>,
+        link: '/sign-in',
+      },
+
+      {
+        text: 'Sign Up',
+        icon: <LabelIcon/>,
+        link: '/sign-up',
+      },
+  ];
+
   return (
-    <Container className={classes.root}>
-      <AppBar className={classes.appBar}>
-        <Toolbar>
-          <Typography variant='h6' className={classes.navTitle}>
+    <div className={classes.root} disableGutters>
+      <AppBar position="fixed" className={classes[navRef.current]}>
+        <Toolbar disableGutters>
+          <Typography variant="h6" className={classes.title} component="h1">
             Beyul Venture
           </Typography>
-          
-          {/* <div className={classes.sectionDesktop}>
-            <NavBtn to='/'>Home</NavBtn>
-            <NavBtn to='/about'>About</NavBtn>
-            <NavBtn to='/blog'>Blog</NavBtn>
-            <NavBtn to='/contact-us'>Contact Us</NavBtn>
-            <NavBtn to='/our-team'>Team</NavBtn>
+          <div className={classes.sectionDesktop}>
+            <NavBtn
+              to='/' className={classes.button} component={NavLink}>Home</NavBtn>
+            <NavBtn disableElevation to='/about' className={classes.button} component={NavLink}>About</NavBtn>
+            <NavBtn to='/blog' className={classes.button} component={NavLink}>Blog</NavBtn>
+            <NavBtn to='/contact-us' className={classes.button} component={NavLink}>Contact</NavBtn>
+            <NavBtn to='/our-team' className={classes.button} component={NavLink}>Team</NavBtn>
+
+            <Button
+              component={NavLink}
+              to='/sign-in'
+              variant='contained'
+              color='#13181e'
+              size='medium'
+              className={classes.login}
+            >
+              Log In
+            </Button>
+
             <Button
               component={NavLink}
               to='/sign-up'
               variant='contained'
               color='primary'
               size='medium'
+              className={classes.signUp}
             >
               Sign Up
             </Button>
+          </div>
 
-            <Button
-              component={NavLink}
-              to='/sign-in'
-              variant='contained'
-              color='primary'
-              size='medium'
-            >
-              Log In
-            </Button>
-          </div> */}
+          <IconButton onClick={toggleDrawer} className={classes.menuIcon} edge="start" color="inherit" aria-label="menu">
+            <SortIcon />
+          </IconButton>
+
+          <Drawer anchor="left" variant="temporary" onClose={toggleDrawer} open={drawerOpen}>
+            <List className={classes.list}>
+                {drawerItems.map(prop => (
+                    <Link className={classes.link} to={prop.link} key={prop.text}>
+                        <ListItem onClick={toggleDrawer} button>
+                            <ListItemIcon style={{color:"#13181e"}}>{prop.icon}</ListItemIcon>
+                            <ListItemText style={{color:"#000000"}}>{prop.text}</ListItemText>
+                        </ListItem>
+                    </Link>
+                ))}
+            </List>
+        </Drawer>
+
+          
         </Toolbar>
       </AppBar>
-    </Container>
-  )
-}
+    </div>
 
-export default Header
+  );
+}
