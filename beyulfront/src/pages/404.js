4132800d0f73ef161b86b11ace5f32e2b8e428c5
Sunil tamang <technicalnepal31@gmail.com>
2021-08-08 21:28:20 +0545

    404 images opacity reduced with dark color overlay

--- beyulfront/src/pages/404.js
+++ beyulfront/src/pages/404.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import Compass from 'components/Compass'
 import FancyLink from 'components/FancyLink'
-import bg404 from 'img/404 (4).jpg'
+import bg404 from 'img/404(1).jpg'
 
 const Wrapper = styled.div`
     background-image: url('${bg404}');
@@ -26,9 +26,9 @@ const FourZeroFour = (props) => {
     return (
         <Wrapper>
             <Compass />
-            <Helptext align='center' variant='h5'>
+            <Helptext align='center' variant='h5' style={{paddingTop:"2rem"}}>
                 It looks like you miscalculated. Click{' '}
-                <FancyLink to='/' barColor='#e0781f'>
+                <FancyLink to='/' barColor='#e0781f' >
                     here
                 </FancyLink>{' '}
                 to return home.
