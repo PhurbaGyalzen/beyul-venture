0d487f40f0a6def62df201779dcaef99d88cf7f0
Sunil tamang <technicalnepal31@gmail.com>
2021-08-08 14:44:27 +0545

    ContactUS images responsiveness fixed

--- beyulfront/src/components/DeveloperCard.js
+++ beyulfront/src/components/DeveloperCard.js
@@ -8,7 +8,9 @@ import {
     Typography,
     Link,
     CardActionArea,
+    CardActions,
     CardContent,
+    Button
 } from '@material-ui/core'
 import FacebookIcon from '@material-ui/icons/Facebook'
 import InstagramIcon from '@material-ui/icons/Instagram'
@@ -24,6 +26,15 @@ const useStyles = makeStyles((theme) => ({
             cursor: 'pointer',
             color:"#694311",
         },
+    },
+
+    mainCard:{
+        height:"100%"
+    },
+
+    cardImg:{
+        width:"100%",
+
     }
 
     
@@ -34,58 +45,58 @@ export const DeveloperCard = (props) => {
     const classes = useStyles()
     return (
         <>
-            <Grid item md={2} sm={5} xs={12} style={{ paddingBottom: '5%' }}>
-                <Card>
-                    <CardMedia
+            <Grid item md={2} sm={5} xs={11} style={{ paddingBottom: '5%' }}>
+                <Card className={classes.mainCard}>
+                    <CardActionArea>
+                        <CardMedia
+                        component="img"
+                        alt="Contemplative Reptile"
+                        height="100%"
                         image={props.data.image}
-                        style={{ height: '10rem', width: '100%' }}
-                    />
+                        title="Contemplative Reptile"
+                        className={classes.cardImg}
+                        />
+                        <CardContent>
+                        <Typography gutterBottom variant="body1" component="h2" style={{color:"#694311",fontSize:"1rem", lineHeight:"1.658rem", fontWeight:"bold"}}>
+                            {props.data.post}
+                        </Typography>
 
-                    <CardContent>
-                        <Box align='Center'>
-                            <Typography variant='body1' color='primary' style={{color:"#ffffff",fontSize:"1rem", lineHeight:"2rem", fontWeight:"bold"}}>
-                                {props.data.post}
-                            </Typography>
-                        </Box>
-                        <Box align='Center' fontWeight='500'>
-                            <Typography variant='body1'>
-                                {props.data.name}
-                            </Typography>
-                        </Box>
-                        <Box align='Center' fontWeight='Light'>
-                            <Typography variant='body1' style={{fontSize:"1rem", lineHeight:"1.658rem", color:"#13181e" }}>
-                                {props.data.bio}
-                            </Typography>
-                        </Box>
-                    </CardContent>
+                        <Typography gutterBottom component="h3" color="textSecondary" style={{fontSize:"0.8rem", lineHeight:"2rem"}}>
+                            {props.data.name}
+                        </Typography>
 
-                    <Box align='Center' pb={1}>
+                        <Typography variant="body2" component="p" style={{color:"#13181e",fontSize:"1rem", lineHeight:"1.658rem"}}>
+                            {props.data.bio}
+                        </Typography>
+                        </CardContent>
                         <Link
-                            href={props.data.fb}
-                            style={{ paddingRight: '4%' }}
-                        >
-                            <FacebookIcon
-                                className={classes.button}
-                            />
-                        </Link>
-                        <Link
-                            href={props.data.instagram}
-                            style={{ paddingRight: '4%' }}
-                        >
-                            <TwitterIcon
-                                className={classes.button}
-                            />
-                        </Link>
-                        <Link
-                            href={props.data.twitter}
-                            style={{ paddingRight: '4%' }}
-                        >
-                            <InstagramIcon
-                                className={classes.button}
-                            />
-                        </Link>
-                    </Box>
+                                href={props.data.fb}
+                                style={{ paddingLeft: '5%', paddingBottom:'5%'}}
+                            >
+                                <FacebookIcon
+                                    className={classes.button}
+                                />
+                            </Link>
+                            <Link
+                                href={props.data.instagram}
+                                style={{ paddingLeft: '5%',  paddingBottom:'5%' }}
+                            >
+                                <InstagramIcon
+                                    className={classes.button}
+                                />
+                            </Link>
+
+                            <Link
+                                href={props.data.twitter}
+                                style={{ paddingLeft: '5%',  paddingBottom:'5%' }}
+                            >
+                                <TwitterIcon
+                                    className={classes.button}
+                                />
+                            </Link>
+                    </CardActionArea>
                 </Card>
+                
             </Grid>
         </>
     )
