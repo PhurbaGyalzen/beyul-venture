9f09dd9468d1804f40bac565bd8be5170c70ec7e
Sunil tamang <technicalnepal31@gmail.com>
2021-08-12 11:07:13 +0545

    A base for itinerary.js file created

--- beyulfront/src/pages/package-detail/Itinerary.js
+++ beyulfront/src/pages/package-detail/Itinerary.js
@@ -8,7 +8,40 @@ import { Button } from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 
 
-//All images import
+//our Itinerary Information
+const itineraries = [
+    {
+        id:1,
+        title:'Day 01: Arrive at Tribhuwan International Airport Kathmandu',
+        description:'No activities are planned for today. You can take a rest and restore your energy for your upcoming adventure. Overnight at Hotel.'
+    },
+
+    {
+        id:2,
+        title:'Day 02: Kathmandu Valley Sightseeing Tour and Preparation for the trek',
+        description:'We will explore around different world heritage sites of ktm valley.'
+    },
+
+    {
+        id:3,
+        title:'Day 03: Fly to Lukla and Trek to Phakding (2652m/8699ft) Walking Distance - 8 km, Duration: 3 hours',
+        description:'No activities are planned for today. You can take a rest and restore your energy for your upcoming adventure. Overnight at Hotel.'
+    },
+
+    {
+        id:4,
+        title:'Day 04: Phakding to Namche Bazaar (3440m/11283) Walking Distance - 10 to 12 km, Duration: 6 hours',
+        description:'No activities are planned for today. You can take a rest and restore your energy for your upcoming adventure. Overnight at Hotel.'
+    },
+
+    {
+        id:5,
+        title:'Day 05: Acclimatization Day – Hike to Everest View Hotel (3,962m/12995ft) Walking Distance - 3 to 4 km, Duration: 3 to 4 hours',
+        description:'No activities are planned for today. You can take a rest and restore your energy for your upcoming adventure. Overnight at Hotel.'
+    },
+];
+    
+    
 
 //Defining custom styles
 const useStyles = makeStyles((theme) => ({
@@ -22,6 +55,8 @@ const useStyles = makeStyles((theme) => ({
 //Defining Itineary component
 export default function Itinerary() {
     const classes = useStyles()
+
+    const[data,setData]=useState(itineraries);
     return (
         <>
             <div className={classes.root}>
