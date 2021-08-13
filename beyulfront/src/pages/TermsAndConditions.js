1e9d4d975c6c401bdaea306afc60c8cdf763d0e9
Sunil tamang <technicalnepal31@gmail.com>
2021-08-13 21:12:18 +0545

    TermsAndConditions sections completed

--- beyulfront/src/pages/TermsAndConditions.js
+++ beyulfront/src/pages/TermsAndConditions.js
@@ -12,34 +12,84 @@ import photo from 'img/termsAndConditions.png';
 const datas=[
     {
         id:0,
-        title:'Ghandruk Ghorepani trekking',
-        content:'Ghandruk Ghorepani trekking is in Annapurna region, which is north-central part of Nepal. Ghorepani and Ghandruk are the two separate villages. Ghorepani is famous for rhododendron forest as well as the fantastic panoramic sunrise, sunset view from Pun Hill 3210 meters which is one hour climbing from Ghorepani.'
+        title:'Down Payment',
+        content:'As per the company policy, you can book your trip paying as little as 20% non-refundable deposits upfront to secure your trip. And you can pay rest of the amount when you arrive to our office in Kathmandu before the trip departure.'
     },
 
     {
         id:1,
-        title:'Ghandruk is a popular tourist',
-        content:'Ghandruk is a popular tourist place for trekkers in Annapurna area of Nepal, with easy trails and various accommodation possibilities, Gurung culture, Monastery, best views of the Annapurna Mountains including a beautiful massif view of Machhapuchhare, call "Fishtail" from Ghandruk.'
+        title:'Payment of the Balance',
+        content:'Your outstanding payment for a trip should be made upon your arrival in Kathmandu. If you would like to pay by credit card, a handling charge of 4% on the outstanding balance is made, while there is no such charge for cash payment. You can also pay by cash such as USD, EURO, SWISS FRANC, and NEPALESE RUPEES. You’re also able to get money from ATM. There is so many ATM machine all around. You just have to mention to your bank, I am going to Nepal before you’re leaving home. So they will make access to your ATM card is working in Nepal. Get money from ATM also very easy.'
     },
 
     {
         id:2,
-        title:'Ghandruk Ghorepani trek offer',
-        content:"Ghandruk Ghorepani trek offer the spectacular mountain scenery along with charming villages inhabited particularly by the Gurungs, Poon & Magars, dense rhododendron forest full of birds and deep sub-tropical valleys, all set below the Annapurna's with the picturesque peak of Machhapuchhare (Fishtail) dominating the skyline. One of the important highlights of this trip is to make a claim on Pun Hill, possibly the Most Spectacular Mountain scrapes on Earth. When the sun rises, it touches the snow-capped summits the Himalayan giants, Dhaulagiri (8,167m) and Annapurna (8,091m) along with a maze of other peaks, there gradually appear, just like magic that your eye could not believe. Beautiful and ornate village of Ghandruk, home of the Annapurna Conservation Area Project, Gurung Museum, culture and custom, Monastery are interesting to visit. Every day at 3PM you have a chance to visit the ACAP project to discover at first hand the conservation work being carried out in Ghandruk village."
-    }
+        title:'Last Minute Booking',
+        content:" The last-minute bookings are also accepted."
+    },
+
+    {
+        id:3,
+        title:'Flight Delay/Cancellation',
+        content:"There might be the possibility of flight delay or cancellation, particularly in Himalayan regions like the Everest region, the Annapurna region, the Kanchenjunga region, and Jumla region that sees bad weather pretty often. Thus, it’s recommended to carry extra cash for food and accommodation in case of delays. Also, if you are traveling to remote areas, it is advised to fit extra days in your itinerary to prepare with some delays and unforeseen conditions. In case of flight delay or cancellation, we will not be responsible for food and accommodation except those in the itinerary."
+    },
+
+    {
+        id:4,
+        title:'Refund',
+        content:" The down payment of 20% will not be refunded in case of cancellation of trip for any other else reason. If you have made the full payment, then you are entitled to refund of all amount minus 20% of the total amount. Still, there are certain charges you might have to incur. To cancel any trip, written notification of your cancellation should be made."
+    },
+
+    {
+        id:5,
+        title:'Injuries and Evacuation',
+        content:" Beyul Venture would not be liable for any health/injury/emotional conditions suffered during the trip. Our packages don’t include any personal insurance, so if you want travel insurance, you will have to get one yourself. We will provide you a personal first-aid kit and larger medicine kit to the group leader, but other than that, we will not be liable for any injury. Thus, it is recommended to get adequate insurance packages, including medical insurance and Helicopter evacuation."
+    },
+
+    {
+        id:6,
+        title:'Unforeseen Circumstances',
+        content:"Our normal refund policy does not apply in the event of natural disasters, War, conflict, unfavorable climate, unforeseen beyond our control etc. In this case deposits are not refundable. It’s best to check your travel insurance and make sure that is cover trip interruption and cancellations. However, we will not be responsible for other expenses you have incurred as a result of this booking. In the event that we will cancel the trip we will work with you to reschedule the trek date at no extra cost. If you decide cancel the trek that we are still running due to one of the above events we will work with you to reschedule the dates at no extra cost as long as you notify us at least 20 days in advance."
+    },
+
+    {
+        id:7,
+        title:'Clients Responsibility',
+        content:" Happyland Treks guide has full authority during your tours. If you commit any illegal act, you will be forced to leave the tour, and no refund will be made. Impact on Your Journey we will make every possible effort to ensure you have a comfortable journey with us. But, you have to understand that the Himalayan countries do not have facilities like the western countries, so you should lower your expectations a lot regarding facilities. The weather might also be a factor that will impact your journey. Especially in Himalayan regions, the weather is a huge problem, so we hope you will deal with patience and proper preparation."
+    },
+
+    {
+        id:8,
+        title:'Papers and Documents',
+        content:"You should present all the necessary documents like Passport, visas, permits, etc. You will be held accountable in case you cannot make trips due to the absence of essential papers and documents."
+    },
+
+    {
+        id:9,
+        title:'Risk and Responsibility',
+        content:"Your safety and entertainment is our top priority. We do not leave any effort to have you enjoy your holiday without any worry. However, there is possibility of natural disasters like floods, landslides, and many other unforeseen problems like flight delay, sickness, or accidents, so we advise you also to be careful. Any extra cost incurring there is your responsibility and should be borne on the spot."
+    },
+
+    {
+        id:10,
+        title:'Travel Insurance',
+        content:"Proof of travel insurance is compulsory before the trek. Standard policies often only cover medical evacuation to 4000m, so make sure the policy you get covers trekking as an activity up to 4000m. Some of the policies in Nepal require you to pay for evacuation upfront and reimburse after the trip ends. Evacuation can cost up to $5000, so make sure to get an insurance policy that covers all this type of payment while traveling. You will mostly require these policies while trekking in the remote regions so that the days will be limited. Happyland Treks strongly recommends you to check specifics with your insurance company before the trip. If you are planning to climb a trekking peak, you will have to get an insurance policy that also covers climbing as an activity. If you are evacuated and want to make an insurance claim, it is mandatory to get a hospital report in Nepal as soon as you get off the mountain. Our itineraries have enough time for you to get acclimatized in higher altitudes; however, it’s important to get the proper medical documentation for claims. You can get a medical report for a fee of about 150 to 200 USD from any hospital. Happyland Treks is not responsible for any evacuation or medical bills during the trip, so make sure you have enough insurance covering these bills. Whatever documentation you need to claim from the insurance company, our team will help you with that."
+    },
 ];
 
 //Defining custom styles
 const useStyles = makeStyles((theme) => ({
     root:{
-        flexGrow:1
+        flexGrow:1,
+        width:"100%",
+        overflow:"hidden"
     },
     
     photoDiv:{
-        width:"100%",
-        height:"50%",
-        position:"relative",
-        boxSizing:"borderBox"
+        width:"100vw",
+        height:"100vh",
+        boxSizing:"borderBox",
+        overflow:"hidden"
     },
 
     photo:{
@@ -47,7 +97,8 @@ const useStyles = makeStyles((theme) => ({
         height:"100%",
         backgroundRepeat:"noRepeat",
         backgroundSize:"cover",
-        backgroundPosition:"center"
+        backgroundPosition:"center",
+
     },
 
     photoText:{
@@ -55,10 +106,6 @@ const useStyles = makeStyles((theme) => ({
         fontWeight:"bold",
         color:"#ffffff",
         letterSpacing:"0.1rem",
-        position:"absolute",
-        top:"50%",
-        left:"50%",
-        transform:"translate(-50%,-50%)"
     },
 
     title:{
@@ -68,7 +115,7 @@ const useStyles = makeStyles((theme) => ({
         color:"#694311",
     },
 
-    description:{
+    content:{
         textAlign:"justify",
         fontSize:"1rem",
         lineHeight:"1.685rem",
@@ -86,27 +133,26 @@ export default function TermsAndConditions() {
         <>
             <div className={classes.root}>
                 <Grid container>
-                    <Grid item>
+                    <Grid item  xs={12}>
                         <div className={classes.photoDiv}>
                             <img src={photo} alt="terms and conditions images" className={classes.photo}/>
-                            <Typography className={classes.photoText}>TERMS AND CONDITIONS</Typography>
                         </div>
                     </Grid>
                 </Grid>
 
                 {termsAndConditions.map((data)=>(
                     <Grid container>
-                        <Box pt={3}>
-                            <Typography className={classes.title}>
-                                {data.title}
-                            </Typography>
+                        <Grid item xs={12}>
+                            <Box p={3} style={{width:"80%", margin:"auto"}}>
+                                <Typography className={classes.title}>
+                                    {data.title}
+                                </Typography>
 
-                            <Typography className={classes.description}>
-                                {data.content}
-                            </Typography>
-
-
-                        </Box>
+                                <Typography className={classes.content}>
+                                    {data.content}
+                                </Typography>
+                            </Box>
+                        </Grid>
                     </Grid>
                 ))}
             </div>
