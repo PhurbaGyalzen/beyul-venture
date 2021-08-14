ca5e86f7ea22ad9a1c4a10cce1a4832916b1968d
zznixt07 <zznixt07@protonmail.com>
2021-08-14 20:17:33 +0545

    credit card does aerobatics

--- beyulfront/src/pages/stripe/CheckoutForm.js
+++ beyulfront/src/pages/stripe/CheckoutForm.js
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from 'react'
-import styled, { keyframes } from 'styled-components'
+import styled, { keyframes, css } from 'styled-components'
 import {
     useStripe,
     useElements,
@@ -17,10 +17,12 @@ const CARD_PLACEHOLDER_COLOR = '#303238'
 const CARD_INVALID_COLOR = '#e5424d'
 const CARD_FONT_SIZE = '16px'
 const CARD_FONT_FAMILY = '"Open Sans", sans-serif'
+const TRANSITION_DURATION = 0.6 // In secs
 
 const PaymentContainer = styled.div`
     max-width: ${(props) => props.maxWidth || CONWIDTH};
     margin: 0 auto;
+    padding-top: 2rem;
     min-height: 100vh;
     display: flex;
     flex-wrap: wrap;
@@ -34,9 +36,11 @@ const CardDetails = styled.div`
 
 const CreditCardCon = styled.div`
     max-width: calc(${CONWIDTH} - 12rem);
+    min-height: 12rem;
     position: relative;
     left: 50%;
     transform: translateX(-50%);
+    transition: transform ${TRANSITION_DURATION}s ease-in-out;
     top: 2rem; /*% wont work without explicit height on parent*/
     background: linear-gradient(
         45deg,
@@ -47,7 +51,7 @@ const CreditCardCon = styled.div`
         #a9afbbd6
     );
     color: white;
-    padding: 0.8rem;
+    /*padding: 0.8rem;*/
     border-radius: 0.7rem;
     font-size: 1.5rem;
     font-weight: 600;
@@ -200,6 +204,9 @@ const CheckoutForm = ({}) => {
     const [canSubmit, setCanSubmit] = useState(false)
     const [errors, setErrors] = useState('')
     const [isLoading, setIsLoading] = useState(false)
+    const [flippedBack, setFlippedBack] = useState(false)
+    const outlinedElem = useRef()
+    const cardRef = useRef()
 
     useEffect(async () => {
         if (!stripe || clientSecret) return
@@ -221,7 +228,7 @@ const CheckoutForm = ({}) => {
             // Stripe.js has not yet loaded.
             return
         }
-
+        await rotateTo('front')
         const result = await stripe.confirmCardPayment(clientSecret, {
             payment_method: {
                 // Get a reference to a mounted CardElement. Elements knows how
@@ -246,8 +253,6 @@ const CheckoutForm = ({}) => {
         }
     }
 
-    const outlinedElem = useRef()
-
     const removeOutline = (elem) => {
         if (elem) {
             elem.style.outline = 'none'
@@ -256,37 +261,64 @@ const CheckoutForm = ({}) => {
     }
 
     const drawOutline = (elem) => {
-        elem.style.outline = '2px solid white'
-        elem.style.outlineOffset = '0.3rem'
+        if (elem) {
+            elem.style.outline = '2px solid white'
+            elem.style.outlineOffset = '0.3rem'
+        }
+    }
+
+    const rotateTo = (side) => {
+        cardRef.current.style.transform = 'translateX(-50%) rotateX(90deg)'
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                cardRef.current.style.transform =
+                    'translateX(-50%) rotateX(0deg)'
+                if (side === 'front') {
+                    setFlippedBack(false)
+                } else {
+                    setFlippedBack(true)
+                }
+                resolve(true)
+            }, TRANSITION_DURATION * 1000)
+        })
     }
 
     const handleFocus = (type) => {
-        let elem
-        switch (type) {
-            case 'num':
-                removeOutline(outlinedElem.current)
-                outlinedElem.current = document.getElementById('num')
-                drawOutline(outlinedElem.current)
-                break
-            case 'expiry':
-                removeOutline(outlinedElem.current)
-                outlinedElem.current = document.getElementById('expiry')
-                drawOutline(outlinedElem.current)
-                break
-            case 'cvc':
-                console.log('cvc clicked')
-                break
-            default:
-                console.log('nothing clicked')
-                removeOutline(outlinedElem.current)
-        }
+        ;(async () => {
+            let elem
+            switch (type) {
+                case 'num':
+                    removeOutline(outlinedElem.current)
+                    if (flippedBack) await rotateTo('front')
+                    // if card should be flipped then gotta wait for element to be
+                    // created first.
+                    outlinedElem.current = document.getElementById('num')
+                    drawOutline(outlinedElem.current)
+                    break
+                case 'expiry':
+                    removeOutline(outlinedElem.current)
+                    if (flippedBack) await rotateTo('front')
+                    outlinedElem.current = document.getElementById('expiry')
+                    drawOutline(outlinedElem.current)
+                    break
+                case 'cvc':
+                    removeOutline(outlinedElem.current)
+                    if (!flippedBack) await rotateTo('back')
+                    outlinedElem.current = document.getElementById('cvv')
+                    drawOutline(outlinedElem.current)
+                    break
+                default:
+                    // rotateTo('front')
+                    removeOutline(outlinedElem.current)
+            }
+        })()
     }
 
     return (
         <PaymentContainer>
             <CardDetails>
-                <CreditCardCon>
-                    <CreditCard />
+                <CreditCardCon ref={cardRef}>
+                    <CreditCard showBack={flippedBack} />
                 </CreditCardCon>
                 <StripeContainer>
                     <StripePaymentForm onSubmit={handleSubmit}>
