7cc021e5afa50d6c86df35f7e7591349fac5b856
PhurbaGyalzen <sphurba861@gmail.com>
2021-08-14 09:30:48 +0545

    slug field removed from admin dashboard

--- beyulback/blog/admin.py
+++ beyulback/blog/admin.py
@@ -4,7 +4,16 @@ from mptt.admin import MPTTModelAdmin
 
 from django.contrib import admin
 
-admin.site.register(Blog)
-admin.site.register(Tag)
+
+class BlogAdmin(admin.ModelAdmin):
+    exclude = ('slug',)
+
+
+class TagAdmin(admin.ModelAdmin):
+    exclude = ('slug',)
+
+
+admin.site.register(Blog, BlogAdmin)
+admin.site.register(Tag, TagAdmin)
 admin.site.register(Comment, MPTTModelAdmin)
 admin.site.register(Clap)
--- beyulback/package/admin.py
+++ beyulback/package/admin.py
@@ -2,7 +2,12 @@ from .models import Package, Review, Photo, Itinerary
 
 from django.contrib import admin
 
+
+class PackageAdmin(admin.ModelAdmin):
+    exclude = ('slug',)
+
+
 admin.site.register(Review)
-admin.site.register(Package)
+admin.site.register(Package, PackageAdmin)
 admin.site.register(Itinerary)
 admin.site.register(Photo)
