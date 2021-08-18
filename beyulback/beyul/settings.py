8210d046c213564ac31ce5af9e5e587683b60dba
PhurbaGyalzen <sphurba861@gmail.com>
2021-08-18 23:06:32 +0545

    More color themes option added

--- beyulback/beyul/settings.py
+++ beyulback/beyul/settings.py
@@ -172,6 +172,45 @@ CKEDITOR_CONFIGS = {
 
     },
 }
+JET_THEMES = [
+    {
+        'theme': 'default',  # theme folder name
+        'color': '#47bac1',  # color of the theme's button in user menu
+        'title': 'Default'  # theme title
+    },
+    {
+        'theme': 'green',
+        'color': '#44b78b',
+        'title': 'Green'
+    },
+    {
+        'theme': 'light-green',
+        'color': '#2faa60',
+        'title': 'Light Green'
+    },
+    {
+        'theme': 'light-violet',
+        'color': '#a464c4',
+        'title': 'Light Violet'
+    },
+    {
+        'theme': 'light-blue',
+        'color': '#5EADDE',
+        'title': 'Light Blue'
+    },
+    {
+        'theme': 'light-gray',
+        'color': '#222',
+        'title': 'Light Gray'
+    }
+]
+
+JET_DEFAULT_THEME = 'green'
+
+JET_SIDE_MENU_COMPACT = True
+
+JET_CHANGE_FORM_SIBLING_LINKS = False
+
 
 # Swapped out the default Django auth.User to CustomUser
 AUTH_USER_MODEL = 'users.CustomUser'
