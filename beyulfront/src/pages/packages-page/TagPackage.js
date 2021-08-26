cd8300e9d541b865cb4ad40baafec7eb9935807d
Nischal Khatri <nischal06khatri@gmail.com>
2021-08-26 23:03:27 +0545

    fixed sign-up toast

---
+++ beyulfront/src/pages/packages-page/TagPackage.js
@@ -0,0 +1,25 @@
+
+import { useState, useEffect } from 'react'
+import {
+    Typography,
+    Grid,
+    Box,
+    Container,
+    makeStyles,
+    Card,
+    CardMedia
+} from '@material-ui/core'
+import { useParams } from 'react-router'
+
+
+export const TagPackage = () => {
+    return (
+        <>
+            <Box>
+                <Container>
+
+                </Container>
+            </Box>
+        </>
+    )
+}
