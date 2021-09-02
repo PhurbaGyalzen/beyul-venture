75d77490b62a53566f11d570247ceaffbd8c71e2
suniltamang <technicalnepal31@gmail.com>
2021-09-02 08:28:26 +0545

    started working in profile seciton

--- beyulfront/src/pages/user-profile/userProfile.js
+++ beyulfront/src/pages/user-profile/userProfile.js
@@ -1,18 +1,37 @@
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
-import Tooltip from '@material-ui/core/Tooltip';
-import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography'
+import Box from '@material-ui/core/Box'
+import Button from '@material-ui/core/Button'
+import Grid from '@material-ui/core/Grid'
+import Tooltip from '@material-ui/core/Tooltip'
+import Avatar from '@material-ui/core/Avatar'
+import { makeStyles } from '@material-ui/core/styles'
+import contactUsImg from 'img/contactUs2.jpg'
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+}))
 
-const userProfile = () =>{
-    return(
+const userProfile = () => {
+    const classes = useStyles
+    return (
         <>
-            <h1>Hello world</h1>
+            <container className={classes.root}>
+                <Grid container>
+                    <Grid item xs={12} sm={12} md={4}>
+                        <div className='sidebar'>
+                            <h1>Hello</h1>
+                        </div>
+                    </Grid>
+
+                    <Grid item xs={12} sm={12} md={8}>
+                        <Typography>Middlebar</Typography>
+                    </Grid>
+                </Grid>
+            </container>
         </>
     )
-
 }
 
 export default userProfile
